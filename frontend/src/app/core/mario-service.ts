75b4494f35b6de38bc2e13c559f433ad53c3f949
Jesus Mendez <jesus@email.com>
2026-05-07 22:38:58 +0200

    modified:   src/app/core/mario-service.ts 	renamed:    src/environments/environments.prod.ts -> src/environments/environments.ts

--- frontend/src/app/core/mario-service.ts
+++ frontend/src/app/core/mario-service.ts
@@ -1,23 +1,24 @@
 import { Personaje, PersonajeResponse } from './../models/personaje';
-
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environments';
 
 @Injectable({
   providedIn: 'root',
 })
 export class MarioService {
 
+   private apiUrl = environment.apiUrl + '/api/';
 
   constructor(private http: HttpClient) {}
 
   getPersonajes(): Observable<any> {
-    return this.http.get('http://mariodex.test/api/personajes');
+    return this.http.get(this.apiUrl + 'personajes');
   }
 
   addPersonajes(Personaje: PersonajeResponse): Observable<PersonajeResponse> {
-    return this.http.post<PersonajeResponse>('http://mariodex.test/api/personajes', Personaje);
+    return this.http.post<PersonajeResponse>(this.apiUrl + 'personajes', Personaje);
   }
 
 
